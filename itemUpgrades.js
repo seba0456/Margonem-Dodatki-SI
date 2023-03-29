f7f2e0e31854b4c9274d6b31de774b6c721a1906
Sebastian Legieziński <46867564+seba0456@users.noreply.github.com>
2023-03-29 21:20:11 +0200

    Update itemUpgrades.js

--- itemUpgrades.js
+++ itemUpgrades.js
@@ -388,7 +388,7 @@
         const isOwned = tipValue.indexOf('Związany z właścicielem') !== -1;
         console.log("Sprawdzanie itemu o nazwie:", itemName, burnUni, isUnique, isHeroic, isLegendary, "IsOwned:", isOwned);
         console.log(isHeroic, isLegendary);
-        if (isHeroic || isLegendary || isOwned) {
+        if (isHeroic || isLegendary) {
             return false;
         } else {
             console.log("zwracam true");
